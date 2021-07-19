007f2e4070aeadcd30f3964622385cdffcd8b823
Jamis March <essention@gmail.com>
2021-07-19 12:58:23 +1000

    Updated requirements.txt and cleaned up some linting for future expansion

--- basic_apps/cdk/src/app.py
+++ basic_apps/cdk/src/app.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3.6
+import os
 
 from aws_cdk import core
 
 from stacks import (
-  minimalStacks as minStacks,
+    main as main,
+    minimalStacks as minStacks,
 )
 
 app = core.App()
 
-minStacks.WaitCondition(app, "WaitCondition")
-minStacks.WaitConditionWithData(app, "WaitConditionWithData")
-minStacks.WaitConditionWithInstance(app, "WaitConditionWithInstance")
+minStacks.WaitCondition(app, 'WaitCondition')
+minStacks.WaitConditionWithData(app, 'WaitConditionWithData')
+minStacks.WaitConditionWithInstance(app, 'WaitConditionWithInstance')
+
+try:
+    cdk_account = os.getenv('CDK_ACCOUNT')
+    cdk_region = os.getenv('CDK_REGION')
+except Exception as e:
+    print(f"The required ENVs CDK_ACCOUNT and CDK_REGION likely haven't been set. Error: {e}")
+
+
+if cdk_account and cdk_region:
+    main.MainInstance(
+        app,
+        'MainInstance',
+        env=core.Environment(
+            account=cdk_account,
+            region=cdk_region,
+        ),
+    )
 
 app.synth()
--- basic_apps/cdk/src/stacks/minimalStacks.py
+++ basic_apps/cdk/src/stacks/minimalStacks.py
@@ -6,37 +6,63 @@ from aws_cdk import (
 
 from .constructs import cfn as con
 
-class WaitCondition(core.Stack):
-  def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-    super().__init__(scope, id, **kwargs)
 
-    cfn.CfnWaitCondition(self, "testWaitCondition")
+class WaitCondition(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        cfn.CfnWaitCondition(self, "testWaitCondition")
+
 
 class WaitConditionWithData(core.Stack):
-  def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-    super().__init__(scope, id, **kwargs, description="A test on sending data to a WaitCondition. Once created to use: Send data output: cfn-signal -d <data> <wait_handle>")
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(
+            scope, id,
+            **kwargs,
+            description="A test on sending data to a WaitCondition. Once created to use: Send data output: cfn-signal -d <data> <wait_handle>"
+        )
 
-    c = con.WaitConditionWithDataConstruct(self, "Blah", count=1, timeout="300")
+        c = con.WaitConditionWithDataConstruct(self, "Blah", count=1, timeout="300")
 
-    core.CfnOutput(self, "Outputs", value=c.getWaitCondition().get_att("Data").to_string(), description="Data for Cloudformation Handle using cfn-signal with data that is outputted into the Outputs")
+        core.CfnOutput(
+            self,
+            "Outputs",
+            value=c.getWaitCondition().get_att("Data").to_string(),
+            description="Data for Cloudformation Handle using cfn-signal with data that is outputted into the Outputs"
+        )
+
 
 class WaitConditionWithInstance(core.Stack):
-  def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-    super().__init__(scope, id, **kwargs, description="A test on sending data to a WaitCondition. Once created to use: Send data output: cfn-signal -d <data> <wait_handle>")
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(
+            scope, id,
+            **kwargs,
+            description="A test on sending data to a WaitCondition. Once created to use: Send data output: cfn-signal -d <data> <wait_handle>"
+        )
 
-    c = con.WaitConditionWithDataConstruct(self, "Blah", count=2, timeout="300")
+        c = con.WaitConditionWithDataConstruct(self, "Blah", count=2, timeout="300")
 
-    instance = ec2.CfnInstance(self, "MainInstance",
-      image_id="ami-f173cc91",
-      key_name="testing",
-      instance_type="t2.micro",
-      subnet_id=core.CfnParameter(self, "SubnetId", type="AWS::EC2::Subnet::Id").to_string(),
-      user_data=core.Fn.base64(core.Fn.sub(
-'''#!/bin/bash
-/opt/aws/bin/cfn-init -s ${{AWS::StackName}} -r MyEC2Instance  --region ${{AWS::Region}}
-/opt/aws/bin/cfn-signal -d "tempdata" {handle}'''.format(handle=c.getWaitHandle().logical_id)
-      )))
+        instance = ec2.CfnInstance(
+            self,
+            "MainInstance",
+            image_id="ami-f173cc91",
+            key_name="testing",
+            instance_type="t2.micro",
+            subnet_id=core.CfnParameter(self, "SubnetId", type="AWS::EC2::Subnet::Id").to_string(),
+            user_data=core.Fn.base64(
+                core.Fn.sub(
+                    '''#!/bin/bash
+                    /opt/aws/bin/cfn-init -s ${{AWS::StackName}} -r MyEC2Instance  --region ${{AWS::Region}}
+                    /opt/aws/bin/cfn-signal -d "tempdata" {handle}'''.format(handle=c.getWaitHandle().logical_id)
+                )
+            )
+        )
 
-    core.CfnOutput(self, "Outputs", value=c.getWaitCondition().get_att("Data").to_string(), description="Data for Cloudformation Handle using cfn-signal with data that is outputted into the Outputs")
+        core.CfnOutput(
+            self,
+            "Outputs",
+            value=c.getWaitCondition().get_att("Data").to_string(),
+            description="Data for Cloudformation Handle using cfn-signal with data that is outputted into the Outputs"
+        )
 
-    c.getWaitCondition().add_depends_on(instance)
+        c.getWaitCondition().add_depends_on(instance)
